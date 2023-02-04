1f365fef28152477b7b437584ccc0e1b53d26f48
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-04 23:42:22 +0530

    adding the value of az count

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,12 @@ variable "aws_region" {
 # Fetch list of all AZs present in a Region
 data "aws_availability_zones" "all" {}
 
+# Calculate AZs Count
+variable "availability_zone_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default = "${output.availability_zone_count.value}"
+}
+
 # Output the Value of AZs
 output "availability_zone_count" {
   value = length(data.aws_availability_zones.all.names)
