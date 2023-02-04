a142fec6b5e43accdcab35ae61c0f9bdd66c3ec3
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-04 23:13:17 +0530

    updating the variables set

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,15 +7,15 @@ variable "aws_region" {
 # Fetch list of all AZs present in a Region
 data "aws_availability_zones" "all" {}
 
-# Calculate AZs Count
-variable "availability_zone_count" {
-  description = "Number of AZs to cover in a given AWS region"
-  default = length(data.aws_availability_zones.all.names)
-}
-
 # Output the Value of AZs
 output "availability_zone_count" {
   value = length(data.aws_availability_zones.all.names)
+}
+
+# Calculate AZs Count
+variable "availability_zone_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default = "${output.availability_zone_count.value}"
 }
 
 # EC2 Default Instance Type
