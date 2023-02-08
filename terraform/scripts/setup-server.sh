c318a5f56aafa0ce6d42049d868d5f40bdfc478a
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-09 02:45:10 +0530

    adding updated code

--- terraform/scripts/setup-server.sh
+++ terraform/scripts/setup-server.sh
@@ -5,14 +5,14 @@ sudo yum install git -y
 sudo amazon-linux-extras install nginx1.12
 sudo systemctl start nginx
 sudo systemctl enable nginx
-mkdir $PWD/apps
-cd $PWD/apps
-git clone https://github.com/shubhamcommits/remix.git
-cd $PWD/remix
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum install yarn -y
-yarn install -g pm2
+sudo yarn global add pm2
+mkdir /apps
+cd /apps
+git clone https://github.com/shubhamcommits/remix.git
+cd /apps/remix
 yarn install
 yarn run build
 pm2 start "yarn run dev" --name "prod-remix-recipe-server"
