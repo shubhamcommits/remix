d9397c0cebed9d7ba77a7993e7a35bcbab2d4590
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-09 01:20:13 +0530

    adding updated code

--- terraform/scripts/setup-server.sh
+++ terraform/scripts/setup-server.sh
@@ -1,41 +1,20 @@
 #!/bin/bash
 
-# Update Yum Repository
 sudo yum update -y
-
-# Install git
 sudo yum install git -y
-
-# Install Nginx
 sudo amazon-linux-extras install nginx1.12
 sudo systemctl start nginx
 sudo systemctl enable nginx
-
-# Install Node.js
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
+source ~/.nvm/nvm.sh
 nvm install 16
-
-# Install pm2 globally
-sudo npm install -g pm2
-
-# Setup Directory
+npm install -g pm2
 mkdir $PWD/apps
-
-# Change Directory
 cd $PWD/apps
-
-# Setup Code
 git clone https://github.com/shubhamcommits/remix.git
-
-# Change Directory to remix
 cd $PWD/remix
-
-# Install Dependencies
 npm install
 npm run build
-
-# Launch Application
 pm2 start "npm run dev" --name "prod-remix-recipe-server"
 
 exit 1
