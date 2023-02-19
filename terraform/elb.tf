808793179f1e00d4554753b20f140ee4a1dec5e1
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-20 03:10:47 +0530

    updating 443 certificates

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -25,15 +25,14 @@ resource "aws_elb" "elb" {
     lb_port             = 443
     lb_protocol         = "https"
     ssl_certificate_id  = "arn:aws:acm:us-east-1:619568494776:certificate/2417c179-3624-438c-8779-7113264bfd76"
-    ssl_policy          = "ELBSecurityPolicy-2016-08"
   }
 
   health_check {
     target              = "HTTP:80/"
-    interval            = 30
+    interval            = 300
     healthy_threshold   = 2
     unhealthy_threshold = 5
-    timeout             = 3
+    timeout             = 60
   }
 
   cross_zone_load_balancing = true
