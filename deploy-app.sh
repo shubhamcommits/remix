918f0a2e1479d29d24f5e4fcd8422a2dd6d8f9e6
Shubham Singh <development.shubhamsingh@gmail.com>
2023-02-19 19:17:38 +0530

    pushing the updated state

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,45 +1,15 @@
 #!/bin/bash
 
-# Set Runtime Variables
-PARAMETER="DEV_RECIPE_REMIX_ENV_VARS"
-REGION="us-east-1"
-APPS_DIR="/home/ec2-user/apps"
+APPS_DIR="/home/ec2-user/apps/remix"
 
-# Check if directory exist
-if [ ! -d $APPS_DIR ]; then
-
-    # Print Outputs
-    echo "Base Directory does not exist"
-    echo "Executing commands for directory creation ..."
-
-    # Change Directory
-    cd $APPS_DIR
-
-    # Clone the Directory
-    git clone https://github.com/shubhamcommits/remix.git
-
-else
-
-    # Print Outputs
-    echo "Base Directory already exists"
-    echo "Continuing ..."
-
-fi
+# Create the Directory
+mkdir -p $APPS_DIR
 
 # Change the Directory
-cd $APPS_DIR/remix
-
-# Get parameters and put it into .env file inside application root
-aws --region $REGION ssm get-parameter --name $PARAMETER  --with-decryption --output text --query Parameter.Value > $APPS_DIR/remix/.env
-
-# Pull the changes
-git pull origin master
+cd $APPS_DIR
 
 # Install the dependencies
-npm install
-
-# Build the application
-npm run build
+yarn install
 
 # Kill all the running application
 pm2 kill
@@ -48,7 +18,7 @@ pm2 kill
 pm2 ls
 
 # Setup the application
-pm2 start "npm run dev" --name "remix-recipe-server"
+pm2 start "yarn run dev" --name "remix-recipe-server"
 
 # Init the application on server Startup
 pm2 startup systemd
